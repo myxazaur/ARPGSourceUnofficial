1851cb2fcc06d1b5bbaf6b9b2d5d84b3ff693d93
Bronit <suslinv037@gmail.com>
2026-02-09 21:36:21 +0300

    try to refactor hooks да, мне было лень и я заставил это делать ии, мне не стыдно

---
+++ src/main/java/com/vivern/arpg/hooks/coloredlightning/ColorOfTheLitBlock.java
@@ -0,0 +1,19 @@
+package com.vivern.arpg.hooks.coloredlightning;
+
+public class ColorOfTheLitBlock {
+   public int redSat;
+   public int greenSat;
+   public int blueSat;
+   public int redPower;
+   public int greenPower;
+   public int bluePower;
+
+   public ColorOfTheLitBlock(int redSat, int greenSat, int blueSat, int redPower, int greenPower, int bluePower) {
+      this.redSat = redSat;
+      this.greenSat = greenSat;
+      this.blueSat = blueSat;
+      this.redPower = redPower;
+      this.greenPower = greenPower;
+      this.bluePower = bluePower;
+   }
+}
