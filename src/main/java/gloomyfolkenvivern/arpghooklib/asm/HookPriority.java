694b6e7b1cead65201bc4df3d4905a4a20903733
Bronit <suslinv037@gmail.com>
2026-02-13 18:23:55 +0300

    fix rebase conflicts

--- src/main/java/gloomyfolkenvivern/arpghooklib/asm/HookPriority.java
+++
@@ -1,9 +0,0 @@
-package gloomyfolkenvivern.arpghooklib.asm;
-
-public enum HookPriority {
-   HIGHEST,
-   HIGH,
-   NORMAL,
-   LOW,
-   LOWEST;
-}
--- src/main/java/gloomyfolkenvivern/arpghooklib/example/coloredlightning/ColoredChunkAccess.java
+++
@@ -1,8 +0,0 @@
-package gloomyfolkenvivern.arpghooklib.example.coloredlightning;
-
-public class ColoredChunkAccess {
-   public ColoredChunk chunk0;
-   public ColoredChunk chunkX;
-   public ColoredChunk chunkZ;
-   public ColoredChunk chunkXZ;
-}
